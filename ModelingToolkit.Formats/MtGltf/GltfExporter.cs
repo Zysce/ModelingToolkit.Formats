db2d1e70534c04e94afd8d438559edcfcd29d909
Zysce <anth.lerouge@gmail.com>
2026-05-20 23:05:15 -0400

    fix usings + add editorconfig and vscode config + file scoped namespaces

--- ModelingToolkit.Formats/MtGltf/GltfExporter.cs
+++ ModelingToolkit.Formats/MtGltf/GltfExporter.cs
@@ -1,172 +1,177 @@
-﻿using System.Numerics;
+﻿using ModelingToolkit.Core;
+using SharpGLTF.Geometry;
+using SharpGLTF.Geometry.VertexTypes;
+using SharpGLTF.Materials;
+using SharpGLTF.Scenes;
+using SkiaSharp;
+using System.Numerics;
 
-namespace ModelingToolkit.Formats.MtGltf
+namespace ModelingToolkit.Formats.MtGltf;
+
+// Structure definition
+using MESH = MeshBuilder<VertexPositionNormal, VertexColor1Texture1, VertexJoints8>;
+using VERTEX = VertexBuilder<VertexPositionNormal, VertexColor1Texture1, VertexJoints8>;
+
+public class GltfExporter
 {
-    // Structure definition
-    using MESH = MeshBuilder<VertexPositionNormal, VertexColor1Texture1, VertexJoints8>;
-    using VERTEX = VertexBuilder<VertexPositionNormal, VertexColor1Texture1, VertexJoints8>;
-
-    public class GltfExporter
+    public static SceneBuilder CreateScene(MtScene mtScene)
     {
-        public static SceneBuilder CreateScene(MtScene mtScene)
+        SceneBuilder scene = new SceneBuilder();
+        bool isSingleModel = mtScene.Models.Count == 1;
+
+        for (int i = 0; i < mtScene.Models.Count; i++)
         {
-            SceneBuilder scene = new SceneBuilder();
-            bool isSingleModel = mtScene.Models.Count == 1;
+            MtModel mtModel = mtScene.Models[i].Model;
 
-            for (int i = 0; i < mtScene.Models.Count; i++)
+            //------------------------------------------------------------------------
+            //  TEXTURES
+            //------------------------------------------------------------------------
+
+            List<ImageBuilder> imgBuilders = new List<ImageBuilder>();
+            for (int j = 0; j < mtModel.Materials.Count; j++)
             {
-                MtModel mtModel = mtScene.Models[i].Model;
-
-                //------------------------------------------------------------------------
-                //  TEXTURES
-                //------------------------------------------------------------------------
-
-                List<ImageBuilder> imgBuilders = new List<ImageBuilder>();
-                for (int j = 0; j < mtModel.Materials.Count; j++)
+                var bmp = mtModel.Materials[j].DiffuseTextureBitmap;
+                if (bmp != null)
                 {
-                    var bmp = mtModel.Materials[j].DiffuseTextureBitmap;
-                    if (bmp != null)
-                    {
-                        using var image = SKImage.FromBitmap(bmp);
-                        using var data = image.Encode(SKEncodedImageFormat.Png, 100);
-                        imgBuilders.Add(ImageBuilder.From(new SharpGLTF.Memory.MemoryImage(data.ToArray())));
-                    }
-                }
-
-                //------------------------------------------------------------------------
-                //  SKELETON
-                //------------------------------------------------------------------------
-
-                // Root node
-                NodeBuilder rootNode = new NodeBuilder(mtModel.Name);
-                rootNode.LocalTransform = Matrix4x4.Identity; // TODO
-
-                // Joint nodes
-                List<NodeBuilder> joints = new List<NodeBuilder>();
-                for (int j = 0; j < mtModel.Joints.Count; j++)
-                {
-                    MtJoint mtJoint = mtModel.Joints[j];
-                    NodeBuilder parentNode = mtJoint.ParentId != null ? joints[mtJoint.ParentId.Value] : rootNode;
-
-                    NodeBuilder joint = parentNode.CreateNode(mtJoint.Name);
-                    joint.LocalTransform = mtJoint.RelativeTransformationMatrix.Value;
-                    joints.Add(joint);
-                }
-                scene.AddNode(rootNode);
-
-                //------------------------------------------------------------------------
-                //  MESHES
-                //------------------------------------------------------------------------
-                for (int j = 0; j < mtModel.Meshes.Count; j++)
-                {
-                    MtMesh mtMesh = mtModel.Meshes[j];
-
-                    // Material
-                    MaterialBuilder matBuilder = new MaterialBuilder(mtMesh.Name + "_mat")
-                                    .WithBaseColor(imgBuilders[mtMesh.MaterialId.Value])
-                                    .WithDoubleSide(false);
-
-                    // Mesh structure definition
-                    MESH meshBuilder = new MESH(mtMesh.Name);
-                    var primBuilder = meshBuilder.UsePrimitive(matBuilder);
-
-                    // Create mesh
-                    foreach (MtFace face in mtMesh.Faces)
-                    {
-                        MtVertex vertex0 = mtMesh.Vertices[face.VertexIndices[0]];
-                        MtVertex vertex1 = mtMesh.Vertices[face.VertexIndices[1]];
-                        MtVertex vertex2 = mtMesh.Vertices[face.VertexIndices[2]];
-
-                        primBuilder.AddTriangle(
-                            VertexToGltf2(vertex0),
-                            VertexToGltf2(vertex1),
-                            VertexToGltf2(vertex2)
-                        );
-                    }
-
-                    scene.AddSkinnedMesh(meshBuilder, Matrix4x4.Identity, joints.ToArray());
+                    using var image = SKImage.FromBitmap(bmp);
+                    using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+                    imgBuilders.Add(ImageBuilder.From(new SharpGLTF.Memory.MemoryImage(data.ToArray())));
                 }
             }
 
-            return scene;
-        }
+            //------------------------------------------------------------------------
+            //  SKELETON
+            //------------------------------------------------------------------------
 
-        internal static VERTEX VertexToGltf2(MtVertex mtVertex)
-        {
-            Vector3 absolutePosition = mtVertex.AbsolutePosition.Value;
-            Vector3 normal = mtVertex.Normals != null ? mtVertex.Normals.Value : Vector3.Zero;
-            Vector4 color = mtVertex.ColorRGBA != null ? mtVertex.ColorRGBA.Value : Vector4.Zero;
-            Vector2 uvCoords = mtVertex.TextureCoordinates != null ? new Vector2(mtVertex.TextureCoordinates.Value.X, 1 - mtVertex.TextureCoordinates.Value.Y) : Vector2.Zero;
-            List<(int, float)> weights = new List<(int, float)>();
-            foreach (var weight in mtVertex.Weights)
+            // Root node
+            NodeBuilder rootNode = new NodeBuilder(mtModel.Name);
+            rootNode.LocalTransform = Matrix4x4.Identity; // TODO
+
+            // Joint nodes
+            List<NodeBuilder> joints = new List<NodeBuilder>();
+            for (int j = 0; j < mtModel.Joints.Count; j++)
             {
-                weights.Add((weight.JointIndex.Value, weight.Weight.Value));
+                MtJoint mtJoint = mtModel.Joints[j];
+                NodeBuilder parentNode = mtJoint.ParentId != null ? joints[mtJoint.ParentId.Value] : rootNode;
+
+                NodeBuilder joint = parentNode.CreateNode(mtJoint.Name);
+                joint.LocalTransform = mtJoint.RelativeTransformationMatrix.Value;
+                joints.Add(joint);
             }
+            scene.AddNode(rootNode);
 
-            VertexPositionNormal vpn = new VertexPositionNormal(absolutePosition, normal);
-            VertexColor1Texture1 vct = new VertexColor1Texture1(color, uvCoords);
-            VertexJoints8 vs = new VertexJoints8(weights.ToArray());
+            //------------------------------------------------------------------------
+            //  MESHES
+            //------------------------------------------------------------------------
+            for (int j = 0; j < mtModel.Meshes.Count; j++)
+            {
+                MtMesh mtMesh = mtModel.Meshes[j];
 
-            return new VERTEX(vpn, vct, vs);
+                // Material
+                MaterialBuilder matBuilder = new MaterialBuilder(mtMesh.Name + "_mat")
+                                .WithBaseColor(imgBuilders[mtMesh.MaterialId.Value])
+                                .WithDoubleSide(false);
+
+                // Mesh structure definition
+                MESH meshBuilder = new MESH(mtMesh.Name);
+                var primBuilder = meshBuilder.UsePrimitive(matBuilder);
+
+                // Create mesh
+                foreach (MtFace face in mtMesh.Faces)
+                {
+                    MtVertex vertex0 = mtMesh.Vertices[face.VertexIndices[0]];
+                    MtVertex vertex1 = mtMesh.Vertices[face.VertexIndices[1]];
+                    MtVertex vertex2 = mtMesh.Vertices[face.VertexIndices[2]];
+
+                    primBuilder.AddTriangle(
+                        VertexToGltf2(vertex0),
+                        VertexToGltf2(vertex1),
+                        VertexToGltf2(vertex2)
+                    );
+                }
+
+                scene.AddSkinnedMesh(meshBuilder, Matrix4x4.Identity, joints.ToArray());
+            }
         }
 
-        /*
-         * NOTES:
-         * animationTrack: Animation name
-         * 
-         * X: Right from viewer
-         * Y: Towards camera
-         * Z: Up
-         * 
-         * NodeBuilder.UseTranslation => Applies absolute translation
-         */
-        //public static void AddAnimation(SceneHelper scene, AnimationBinary animation, string animationName = "Anim")
-        //{
-        //    List<KeyWrapper> initialPoses = new List<KeyWrapper>();
-        //
-        //    // INITIAL POSE
-        //    foreach (Motion.InitialPose initialPose in animation.MotionFile.InitialPoses)
-        //    {
-        //        initialPoses.Add(new KeyWrapper { Value = initialPose.Value, JointId = initialPose.BoneId, Channel = initialPose.ChannelValue });
-        //
-        //        NodeBuilder jointNode = scene.Joints[initialPose.BoneId];
-        //
-        //        if (initialPose.ChannelValue == Motion.Channel.TRANSLATION_X)
-        //        {
-        //            CURVE_VECTOR3 cur = jointNode.UseTranslation(animationName);
-        //            cur.WithPoint(0, cur.GetPoint(0) + new Vector3(initialPose.Value, 0, 0));
-        //        }
-        //        if (initialPose.ChannelValue == Motion.Channel.TRANSLATION_Y)
-        //        {
-        //            CURVE_VECTOR3 cur = jointNode.UseTranslation(animationName);
-        //            cur.WithPoint(0, cur.GetPoint(0) + new Vector3(0, initialPose.Value, 0));
-        //        }
-        //        if (initialPose.ChannelValue == Motion.Channel.TRANSLATION_Z)
-        //        {
-        //            CURVE_VECTOR3 cur = jointNode.UseTranslation(animationName);
-        //            cur.WithPoint(0, cur.GetPoint(0) + new Vector3(0, 0, initialPose.Value));
-        //        }
-        //        if (initialPose.ChannelValue == Motion.Channel.ROTATION_X)
-        //        {
-        //            CURVE_QUATERNION cur = jointNode.UseRotation(animationName);
-        //            cur.WithPoint(0, cur.GetPoint(0) + Quaternion.CreateFromAxisAngle(Vector3.UnitX, initialPose.Value));
-        //        }
-        //        if (initialPose.ChannelValue == Motion.Channel.ROTATION_Y)
-        //        {
-        //            CURVE_QUATERNION cur = jointNode.UseRotation(animationName);
-        //            cur.WithPoint(0, cur.GetPoint(0) + Quaternion.CreateFromAxisAngle(Vector3.UnitY, initialPose.Value));
-        //        }
-        //        if (initialPose.ChannelValue == Motion.Channel.ROTATION_Z)
-        //        {
-        //            CURVE_QUATERNION cur = jointNode.UseRotation(animationName);
-        //            cur.WithPoint(0, cur.GetPoint(0) + Quaternion.CreateFromAxisAngle(Vector3.UnitZ, initialPose.Value));
-        //        }
-        //
-        //        //CURVE_VECTOR3 cur123 = jointNode.UseTranslation(animationName);
-        //        //Vector3 check = cur123.GetPoint(0);
-        //    }
-        //
-        //    var modelCheck = scene.Scene.ToGltf2();
-        //}
+        return scene;
     }
+
+    internal static VERTEX VertexToGltf2(MtVertex mtVertex)
+    {
+        Vector3 absolutePosition = mtVertex.AbsolutePosition.Value;
+        Vector3 normal = mtVertex.Normals != null ? mtVertex.Normals.Value : Vector3.Zero;
+        Vector4 color = mtVertex.ColorRGBA != null ? mtVertex.ColorRGBA.Value : Vector4.Zero;
+        Vector2 uvCoords = mtVertex.TextureCoordinates != null ? new Vector2(mtVertex.TextureCoordinates.Value.X, 1 - mtVertex.TextureCoordinates.Value.Y) : Vector2.Zero;
+        List<(int, float)> weights = new List<(int, float)>();
+        foreach (var weight in mtVertex.Weights)
+        {
+            weights.Add((weight.JointIndex.Value, weight.Weight.Value));
+        }
+
+        VertexPositionNormal vpn = new VertexPositionNormal(absolutePosition, normal);
+        VertexColor1Texture1 vct = new VertexColor1Texture1(color, uvCoords);
+        VertexJoints8 vs = new VertexJoints8(weights.ToArray());
+
+        return new VERTEX(vpn, vct, vs);
+    }
+
+    /*
+     * NOTES:
+     * animationTrack: Animation name
+     * 
+     * X: Right from viewer
+     * Y: Towards camera
+     * Z: Up
+     * 
+     * NodeBuilder.UseTranslation => Applies absolute translation
+     */
+    //public static void AddAnimation(SceneHelper scene, AnimationBinary animation, string animationName = "Anim")
+    //{
+    //    List<KeyWrapper> initialPoses = new List<KeyWrapper>();
+    //
+    //    // INITIAL POSE
+    //    foreach (Motion.InitialPose initialPose in animation.MotionFile.InitialPoses)
+    //    {
+    //        initialPoses.Add(new KeyWrapper { Value = initialPose.Value, JointId = initialPose.BoneId, Channel = initialPose.ChannelValue });
+    //
+    //        NodeBuilder jointNode = scene.Joints[initialPose.BoneId];
+    //
+    //        if (initialPose.ChannelValue == Motion.Channel.TRANSLATION_X)
+    //        {
+    //            CURVE_VECTOR3 cur = jointNode.UseTranslation(animationName);
+    //            cur.WithPoint(0, cur.GetPoint(0) + new Vector3(initialPose.Value, 0, 0));
+    //        }
+    //        if (initialPose.ChannelValue == Motion.Channel.TRANSLATION_Y)
+    //        {
+    //            CURVE_VECTOR3 cur = jointNode.UseTranslation(animationName);
+    //            cur.WithPoint(0, cur.GetPoint(0) + new Vector3(0, initialPose.Value, 0));
+    //        }
+    //        if (initialPose.ChannelValue == Motion.Channel.TRANSLATION_Z)
+    //        {
+    //            CURVE_VECTOR3 cur = jointNode.UseTranslation(animationName);
+    //            cur.WithPoint(0, cur.GetPoint(0) + new Vector3(0, 0, initialPose.Value));
+    //        }
+    //        if (initialPose.ChannelValue == Motion.Channel.ROTATION_X)
+    //        {
+    //            CURVE_QUATERNION cur = jointNode.UseRotation(animationName);
+    //            cur.WithPoint(0, cur.GetPoint(0) + Quaternion.CreateFromAxisAngle(Vector3.UnitX, initialPose.Value));
+    //        }
+    //        if (initialPose.ChannelValue == Motion.Channel.ROTATION_Y)
+    //        {
+    //            CURVE_QUATERNION cur = jointNode.UseRotation(animationName);
+    //            cur.WithPoint(0, cur.GetPoint(0) + Quaternion.CreateFromAxisAngle(Vector3.UnitY, initialPose.Value));
+    //        }
+    //        if (initialPose.ChannelValue == Motion.Channel.ROTATION_Z)
+    //        {
+    //            CURVE_QUATERNION cur = jointNode.UseRotation(animationName);
+    //            cur.WithPoint(0, cur.GetPoint(0) + Quaternion.CreateFromAxisAngle(Vector3.UnitZ, initialPose.Value));
+    //        }
+    //
+    //        //CURVE_VECTOR3 cur123 = jointNode.UseTranslation(animationName);
+    //        //Vector3 check = cur123.GetPoint(0);
+    //    }
+    //
+    //    var modelCheck = scene.Scene.ToGltf2();
+    //}
 }
--- ModelingToolkit.Formats/MtPorter.cs
+++ ModelingToolkit.Formats/MtPorter.cs
@@ -1,87 +1,90 @@
-﻿namespace ModelingToolkit.Formats
+﻿using ModelingToolkit.Core;
+using SharpGLTF.Scenes;
+using SkiaSharp;
+
+namespace ModelingToolkit.Formats;
+
+public class MtPorter
 {
-    public class MtPorter
+    public static MtScene ImportScene(string filepath)
     {
-        public static MtScene ImportScene(string filepath)
+        string extension = Path.GetExtension(filepath).ToLower();
+        if (extension == ".fbx")
         {
-            string extension = Path.GetExtension(filepath).ToLower();
-            if (extension == ".fbx")
-            {
-                return MtAssimp.AssimpImporter.ImportScene(filepath);
-            }
-            // TODO
-            //else if(extension == ".gltf" || extension == ".glb")
-            //{
-            //    return MtGltf.GltfImporter.Import(new byte[0]);
-            //}
-            else
-            {
-                throw new Exception("Invalid file type");
-            }
+            return MtAssimp.AssimpImporter.ImportScene(filepath);
         }
-
-        public static void ExportScene(MtScene scene, string filepath, FileType fileType)
+        // TODO
+        //else if(extension == ".gltf" || extension == ".glb")
+        //{
+        //    return MtGltf.GltfImporter.Import(new byte[0]);
+        //}
+        else
         {
-            if (fileType == FileType.FBX)
-            {
-                Assimp.Scene assimpScene = MtAssimp.AssimpExporter.ExportScene(scene);
-                Assimp.AssimpContext context = new Assimp.AssimpContext();
-                context.ExportFile(assimpScene, filepath, "fbx");
-                ExportTextures(scene, filepath);
-            }
-            else if (fileType == FileType.GLTF)
-            {
-                SceneBuilder sb = MtGltf.GltfExporter.CreateScene(scene);
-                sb.ToGltf2().SaveGLTF(filepath);
-            }
-            else if (fileType == FileType.GLB)
-            {
-                SceneBuilder sb = MtGltf.GltfExporter.CreateScene(scene);
-                sb.ToGltf2().SaveGLB(filepath);
-            }
-            else
-            {
-                throw new Exception("Invalid file type");
-            }
+            throw new Exception("Invalid file type");
         }
+    }
 
-        /*
-         * Currently supported file types
-         */
-        public enum FileType
+    public static void ExportScene(MtScene scene, string filepath, FileType fileType)
+    {
+        if (fileType == FileType.FBX)
         {
-            FBX,
-            GLB,
-            GLTF
+            Assimp.Scene assimpScene = MtAssimp.AssimpExporter.ExportScene(scene);
+            Assimp.AssimpContext context = new Assimp.AssimpContext();
+            context.ExportFile(assimpScene, filepath, "fbx");
+            ExportTextures(scene, filepath);
         }
-
-        /*
-         * Exports the textures from the given scene to the given model filepath
-         */
-        public static void ExportTextures(MtScene scene, string filePath, string prefix = "")
+        else if (fileType == FileType.GLTF)
         {
-            foreach (MtScene.MtModelNode modelNode in scene.Models)
-            {
-                ExportTextures(modelNode.Model, filePath, prefix);
-            }
+            SceneBuilder sb = MtGltf.GltfExporter.CreateScene(scene);
+            sb.ToGltf2().SaveGLTF(filepath);
         }
-
-        /*
-         * Exports the textures from the given model to the given model filepath
-         */
-        public static void ExportTextures(MtModel model, string filePath, string prefix = "")
+        else if (fileType == FileType.GLB)
         {
-            for (int i = 0; i < model.Materials.Count; i++)
+            SceneBuilder sb = MtGltf.GltfExporter.CreateScene(scene);
+            sb.ToGltf2().SaveGLB(filepath);
+        }
+        else
+        {
+            throw new Exception("Invalid file type");
+        }
+    }
+
+    /*
+     * Currently supported file types
+     */
+    public enum FileType
+    {
+        FBX,
+        GLB,
+        GLTF
+    }
+
+    /*
+     * Exports the textures from the given scene to the given model filepath
+     */
+    public static void ExportTextures(MtScene scene, string filePath, string prefix = "")
+    {
+        foreach (MtScene.MtModelNode modelNode in scene.Models)
+        {
+            ExportTextures(modelNode.Model, filePath, prefix);
+        }
+    }
+
+    /*
+     * Exports the textures from the given model to the given model filepath
+     */
+    public static void ExportTextures(MtModel model, string filePath, string prefix = "")
+    {
+        for (int i = 0; i < model.Materials.Count; i++)
+        {
+            string textureFilePath = Path.Combine(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(model.Materials[i].DiffuseTextureFileName) + ".png");
+            var bmp = model.Materials[i].DiffuseTextureBitmap;
+            if (bmp != null)
             {
-                string textureFilePath = Path.Combine(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(model.Materials[i].DiffuseTextureFileName) + ".png");
-                var bmp = model.Materials[i].DiffuseTextureBitmap;
-                if (bmp != null)
-                {
-                    using var image = SKImage.FromBitmap(bmp);
-                    using var data = image.Encode(SKEncodedImageFormat.Png, 100);
-                    using var fs = File.OpenWrite(textureFilePath);
-                    data.SaveTo(fs);
-                }
+                using var image = SKImage.FromBitmap(bmp);
+                using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+                using var fs = File.OpenWrite(textureFilePath);
+                data.SaveTo(fs);
             }
         }
     }
